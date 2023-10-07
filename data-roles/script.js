726223a626706cc94f889b09e02c73c4d3b15d58
blong206b <blong206b@gmail.com>
2023-10-07 17:01:05 -0400

    Update script.js

--- data-roles/script.js
+++ data-roles/script.js
@@ -2,12 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
   let currentNode = 0;
 
   function moveToNode(node) {
+    // Remove the 'active' class from the current node
     document.getElementById(`node-${currentNode}`).classList.remove('active');
+    
+    // Update the current node to the next node
     currentNode = node;
+
+    // Add the 'active' class to the new current node
     document.getElementById(`node-${currentNode}`).classList.add('active');
   }
 
+  // Make the moveToNode function globally accessible so the HTML can use it
   window.moveToNode = moveToNode;
 
-  moveToNode(0);  // Initialize to start node
+  // Initialize to the start node
+  moveToNode(0);
 });
