3d08a8b9faeea7eb67d30035cd380028117d834c
blong206b <blong206b@gmail.com>
2023-10-07 13:45:09 -0400

    security roles

---
+++ cyber-roles/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let currentNode = 0;
+
+  function moveToNode(node) {
+    document.getElementById(`node-${currentNode}`).classList.remove('active');
+    currentNode = node;
+    document.getElementById(`node-${currentNode}`).classList.add('active');
+  }
+  
+  window.moveToNode = moveToNode;
+  moveToNode(0);  // Start node
+});
