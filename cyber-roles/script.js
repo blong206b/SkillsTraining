bb51b491c404a7bcc658d3d228ec47dbbe1a9735
blong206b <blong206b@gmail.com>
2023-10-07 14:10:53 -0400

    prettify the roles

--- cyber-roles/script.js
+++ cyber-roles/script.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   let currentNode = 0;
+  const historyStack = [];
 
   function moveToNode(node) {
     document.getElementById(`node-${currentNode}`).classList.remove('active');
+    historyStack.push(currentNode);
     currentNode = node;
     document.getElementById(`node-${currentNode}`).classList.add('active');
   }
-  
+
   function moveBack() {
     if (historyStack.length === 0) return;
     document.getElementById(`node-${currentNode}`).classList.remove('active');
@@ -14,6 +16,16 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById(`node-${currentNode}`).classList.add('active');
   }
 
+  function startOver() {
+    document.getElementById(`node-${currentNode}`).classList.remove('active');
+    historyStack.length = 0;  // Clear the history stack
+    currentNode = 0;  // Reset to the starting node
+    document.getElementById(`node-${currentNode}`).classList.add('active');
+  }
+
   window.moveToNode = moveToNode;
-  moveToNode(0);  // Start node
+  window.moveBack = moveBack;
+  window.startOver = startOver;
+
+  moveToNode(0);  // Initialize to start node
 });
