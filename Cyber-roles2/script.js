0aef1b1eddca393033f26d05def1d546e4eacfbb
blong206b <blong206b@gmail.com>
2023-10-08 18:47:38 -0400

    cleanup

---
+++ Cyber-roles2/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let currentNode = 0;
+  const historyStack = [];
+
+  function moveToNode(node) {
+    document.getElementById(`node-${currentNode}`).classList.remove('active');
+    historyStack.push(currentNode);
+    currentNode = node;
+    document.getElementById(`node-${currentNode}`).classList.add('active');
+  }
+
+  function moveBack() {
+    if (historyStack.length === 0) return;
+    document.getElementById(`node-${currentNode}`).classList.remove('active');
+    currentNode = historyStack.pop();
+    document.getElementById(`node-${currentNode}`).classList.add('active');
+  }
+
+  function startOver() {
+    document.getElementById(`node-${currentNode}`).classList.remove('active');
+    historyStack.length = 0;  // Clear the history stack
+    currentNode = 0;  // Reset to the starting node
+    document.getElementById(`node-${currentNode}`).classList.add('active');
+  }
+
+  // Add more functions for specific roles and transitions as needed
+
+  window.moveToNode = moveToNode;
+  window.moveBack = moveBack;
+  window.startOver = startOver;
+
+  moveToNode(0);  // Initialize to start node
+});
